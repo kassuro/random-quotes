d0aafe60ce90b307ecf91b817d31a20d15dcd2fe
kassuro <contact@kassuro.de>
2018-04-30 20:23:11 +0200

    removing default component. Adds Quote component with minimal layout

---
+++ src/components/Quote.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="quote">
+    <h1>{{ quoteText }}</h1>
+    <h2>{{ quoteAuthor }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            quoteText: 'test text',
+            quoteAuthor: 'Some Name',
+        };
+    },
+};
+</script>
+
+<style>
+    #quote {
+        border: 1px solid red;
+        height: 300px;
+        margin: 10% auto;
+        min-height: 200px;
+        width: 80%;
+    }
+    #quote h1 {
+        margin-top: 8%;
+        text-align: center;
+    }
+
+    #quote h2 {
+        text-align: right;
+        margin-right: 5%;
+        font-style: italic;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Quote from '@/components/Quote';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'Quote',
+            component: Quote
+        }
+    ]
+});
