75139e0c45e2535324de39d4613dc146628f7f5a
kassuro <contact@kassuro.de>
2018-05-01 12:34:20 +0200

    adds a little bit of styling to quote component

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -33,20 +33,51 @@ export default {
 
 <style>
     #quote {
-        border: 1px solid red;
+        background: #3F51B5;
+        box-shadow: 3px 3px 8px rgba(0, 0, 0, .3),
+            -3px 1px 8px rgba(0, 0, 0, .3);
+        color: #FFF;
         height: 300px;
         margin: 10% auto;
-        min-height: 200px;
+        min-height: 300px;
+        padding: 15px;
+        position: relative;
         width: 80%;
     }
     #quote h1 {
+        font-family: 'Tangerine', cursive;
+        font-size: 2.2rem;
         margin-top: 8%;
+        padding: 10px;
         text-align: center;
     }
 
     #quote h2 {
         text-align: right;
         margin-right: 5%;
+        font-family: 'Roboto', sans-serif;
+        font-size: 1rem;
         font-style: italic;
+        font-weight: 400;
     }
+
+    #quote button {
+        background: #7986CB;
+        box-shadow: 1px 1px 5px rgba(0, 0, 0, .3),
+            -1px 1px 5px rgba(0, 0, 0, .3);
+        border: none;
+        bottom: 30px;
+        color: #FFF;
+        font-family: 'Roboto', sans-serif;
+        height: 30px;
+        position: absolute;
+        right: 15px;
+        width: 100px;
+    }
+
+    #quote button:active {
+        box-shadow: none;
+    }
+
+
 </style>
